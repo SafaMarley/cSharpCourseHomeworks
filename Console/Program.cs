a20f9ef8e698bb9cddaa51c4a161246424d64f9c
Safa <safaktrcgl@gmail.com>
2021-10-10 15:56:53 +0300

    Console project added

---
+++ Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("Your name:");
+        string name = Console.ReadLine();
+        Console.WriteLine("Your surname:");
+        string surname = Console.ReadLine();
+        Console.WriteLine("Hi " + name + " " + surname);
+    }
+}
