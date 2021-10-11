d387c6c4fba2be8d429535dc9fefb26007c990f3
Safa <safaktrcgl@gmail.com>
2021-10-11 12:12:42 +0300

    Conditions added

---
+++ Conditions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Conditions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if(time >= 6 && time <= 11) {
+                Console.WriteLine("Have a nice morning.");
+            }
+            if(time <= 18) {
+                Console.WriteLine("Have a nice day.");
+            }
+            else
+            {
+                Console.WriteLine("Have a nice night.");
+            }
+
+            string greeting = time >= 6 && time <= 11 ? "Have a nice morning." : time <= 18 ? "Have a nice day." : "Have a nice night.";
+            Console.WriteLine(greeting);
+
+        }
+    }
+}
